cd0e577c012be1674b9ded99256dadf7b73e4acf
Francois <francois.thullier98@gmail.com>
2025-04-07 13:38:08 +0200

    Created Header component

---
+++ src/Header/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Header.module.css";
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>
+        Coffee <span>Yeya</span>
+      </h1>
+      <nav className={styles.navbar}>
+        <ul>
+          <li>
+            {" "}
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="../shop">Shop</Link>
+          </li>
+          <li>About</li>
+          <li>
+            <Link to="/cart">
+              <img src="../src/img/cart-outline.svg" alt="Cart" />
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
