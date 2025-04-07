61cded0f2a9d3af0a807ebc22756dacd84dcde23
Francois <francois.thullier98@gmail.com>
2025-04-07 13:39:30 +0200

    Removed hard coded footer and header

--- src/Shop/Shop.jsx
+++ src/Shop/Shop.jsx
@@ -1,43 +1,54 @@
 import React from "react";
 import styles from "../Home/Home.module.css";
 import styles2 from "../Shop/Shop.module.css";
-import { Link } from "react-router-dom";
 import Item from "../Item/Item";
-
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import { useState, useEffect } from "react";
 export default function Shop() {
+  const [cartArray, setCartArray] = useState([]);
+
+  const displaySelectedItem = (itemName, quantity, price) => {
+    const total = quantity * price;
+    console.log(`You bought ${quantity} of ${itemName} for ${total} USD`);
+    setCartArray((prevCart) => [
+      ...prevCart,
+      {
+        productName: itemName,
+        quantity: quantity,
+        price: price,
+      },
+    ]);
+  };
+  useEffect(() => {
+    console.log("Cart updated:", cartArray);
+  }, [cartArray]);
   return (
     <div className={styles.home}>
-      <div className={styles.header}>
-        <h1>Coffee Yeya</h1>
-        <nav className={styles.navbar}>
-          <ul>
-            <li>
-              <Link to="../">Home</Link>
-            </li>
-            <li>Shop</li>
-            <li>About</li>
-
-            <img src="../src/img/cart-outline.svg" alt="" />
-          </ul>
-        </nav>
-      </div>
+      <Header />
 
       <div className={styles2.main}>
         <h2>Items</h2>
         <div className={styles2.itemList}>
-          <Item></Item>
-          <Item itemName="Americano" price={15}></Item>
-          <Item itemName="Lungo" price={5}></Item>
+          <Item
+            itemName="Espresso"
+            itemPrice={4.95}
+            onAddToCart={displaySelectedItem}
+          ></Item>
+          <Item
+            itemName="Americano"
+            itemPrice={9.99}
+            onAddToCart={displaySelectedItem}
+          ></Item>
+          <Item
+            itemName="Lungo"
+            itemPrice={4.95}
+            onAddToCart={displaySelectedItem}
+          ></Item>
         </div>
       </div>
-      <div className={styles.footer}>
-        <div className={styles.socials}>
-          <h2>Siguenos en redes sociales!</h2>
-          <img src="../src/img/icons8-instagram-24.png" alt="" />
-          <img src="../src/img/icons8-tiktok-24.png" alt="" />
-          <img src="../src/img/icons8-facebook-24.png" alt="" />
-        </div>
-      </div>
+
+      <Footer />
     </div>
   );
 }
