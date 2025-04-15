c0d8bc6b9fe9b84ee79f204998aa96650506fb51
Francois <francois.thullier98@gmail.com>
2025-04-15 08:56:01 +0200

    Added a button image to delete an item from the cart

--- src/CartItem/CartItem.jsx
+++ src/CartItem/CartItem.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import styles from "./CartItem.module.css";
 
-export default function CartItem({ itemName, quantity, price }) {
+export default function CartItem({ itemName, quantity, price, removeItem }) {
   const totalPrice = quantity * price;
+
+  // const deleteItemFromCart = () => {
+  //   console.log(`You deleted the item ${itemName}`);
+  // };
   return (
     <div className={styles.cartItem}>
       <h4>{itemName}</h4>
       <p>Quantity : {quantity}</p>
       <p>{totalPrice}</p>
+      <img
+        src="../src/img/delete.svg"
+        alt=""
+        onClick={() => {
+          removeItem(itemName);
+        }}
+      />
     </div>
   );
 }
