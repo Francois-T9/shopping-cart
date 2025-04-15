0f06744b9eafa007d7f3ceabdc45808326b14b97
Francois <francois.thullier98@gmail.com>
2025-04-15 08:54:58 +0200

    Removed router

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
 import "./index.css";
-import Home from "../src/Home/Home.jsx";
-import Shop from "../src/Shop/Shop.jsx";
-import Cart from "./Cart/Cart.jsx";
-
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  {
-    path: "shop",
-    element: <Shop />,
-  },
-  {
-    path: "cart",
-    element: <Cart />,
-  },
-]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>
 );
