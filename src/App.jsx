86277f328efe8f18cdb73885ce75276fcdda6d10
Francois <francois.thullier98@gmail.com>
2025-04-22 14:14:58 +0200

    Removed share state, replaced router provider with context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,28 @@
-import React, { useState, useEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { CartProvider } from "./CartContext";
 import Home from "../src/Home/Home.jsx";
 import Shop from "../src/Shop/Shop.jsx";
 import Cart from "../src/Cart/Cart.jsx";
 
 export default function App() {
-  const [cartArray, setCartArray] = useState([]);
-
-  useEffect(() => {
-    console.log("Cart updated:", cartArray); // This will log every time cartArray is updated
-  }, [cartArray]);
-
-  const removeItemFromCart = (itemName) => {
-    console.log("Removing item:", itemName);
-
-    setCartArray((prevCartArray) => {
-      const updatedCart = prevCartArray.filter(
-        (item) => item.productName !== itemName
-      );
-      return updatedCart;
-    });
-  };
-
-  const removeAllCartItems = () => {
-    console.log("All items removed");
-    setCartArray([]); // Clear the cart
-  };
-
   const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
+    {
+      path: "/",
+      element: <Home />,
+    },
     {
       path: "shop",
-      element: (
-        <Shop
-          setCartArray={setCartArray}
-          removeItemFromCart={removeItemFromCart}
-        />
-      ),
+      element: <Shop />,
     },
     {
       path: "cart",
-      element: (
-        <Cart
-          key={JSON.stringify(cartArray)}
-          cartArray={cartArray}
-          removeItemFromCart={removeItemFromCart}
-          removeAllItems={removeAllCartItems}
-        />
-      ),
+      element: <Cart />,
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <CartProvider>
+      <RouterProvider router={router} />
+    </CartProvider>
+  );
 }
