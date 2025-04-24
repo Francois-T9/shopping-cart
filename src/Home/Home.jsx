624700615b0f3791471033814a0470f8a6e05f11
Francois <francois.thullier98@gmail.com>
2025-04-24 13:58:33 +0200

    Added lorem ipsum

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -5,15 +5,23 @@ import Footer from "../Footer/Footer";
 
 export default function Home() {
   return (
-    <div className={styles.home}>
+    <div>
       <Header />
 
       <div className={styles.main}>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-          aliquid inventore aperiam accusamus tempora nam porro ab eius quidem
-          voluptates dolore sequi, cum animi, dolorum, ipsum architecto
-          aspernatur nisi quod?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, hic
+          repellendus incidunt doloribus beatae quasi eos labore similique nulla
+          laboriosam exercitationem ea corrupti ipsam quaerat voluptatem
+          blanditiis porro culpa itaque voluptate totam non molestiae eum! Sunt
+          libero explicabo eveniet, autem commodi aliquam quidem similique non
+          vitae fuga at deserunt culpa ex qui, beatae expedita ad perspiciatis
+          molestias dolor dolorem eaque dolorum odit ratione. Rerum ex
+          dignissimos nostrum esse repudiandae perferendis, eos natus, ut
+          exercitationem impedit similique inventore error officia. Consequuntur
+          odio aspernatur velit. Magni nulla ipsum vero obcaecati exercitationem
+          recusandae nam? Perspiciatis nisi voluptate magni, fugiat architecto
+          aut accusantium voluptates!
         </p>
         <img
           src="../src/img/pexels-aysenurhamra-68268085-10338179.jpg"
