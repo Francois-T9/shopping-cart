c049aa8e8c947bd8766ce1165739c7a6ddc457c9
Francois <francois.thullier98@gmail.com>
2025-04-24 13:56:46 +0200

    Lifted API state from Shop to parent

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import { CartProvider } from "./CartContext";
 import Home from "../src/Home/Home.jsx";
 import Shop from "../src/Shop/Shop.jsx";
 import Cart from "../src/Cart/Cart.jsx";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const [apiItems, setApiItems] = useState([]);
+  useEffect(() => {
+    if (apiItems.length === 0) {
+      fetch("https://fakestoreapi.com/products")
+        .then((res) => res.json())
+        .then((json) => setApiItems(json.slice(0, 8)));
+    }
+  }, [apiItems]);
   const router = createBrowserRouter([
     {
       path: "/",
@@ -12,7 +21,7 @@ export default function App() {
     },
     {
       path: "shop",
-      element: <Shop />,
+      element: <Shop apiItems={apiItems} />,
     },
     {
       path: "cart",
