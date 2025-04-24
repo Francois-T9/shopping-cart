71167898231880d14912dd256999039eb54ad192
Francois <francois.thullier98@gmail.com>
2025-04-24 13:57:33 +0200

    Added several fake links to fill the footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -5,10 +5,30 @@ export default function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.socials}>
-        <h2>Siguenos en redes sociales!</h2>
-        <img src="../src/img/icons8-instagram-24.png" alt="" />
-        <img src="../src/img/icons8-tiktok-24.png" alt="" />
-        <img src="../src/img/icons8-facebook-24.png" alt="" />
+        <h3>Redes sociales</h3>
+        <img src="../src/img/icons8-instagram-24.png" alt="Instagram" />
+        <img src="../src/img/icons8-tiktok-24.png" alt="TikTok" />
+        <img src="../src/img/icons8-facebook-24.png" alt="Facebook" />
+
+        <img src="../src/img/github (1).svg" alt="Twitter" />
+      </div>{" "}
+      <div className={styles.products}>
+        <h3>Nuestros productos</h3>
+        <p>Playeras</p>
+        <p>Collares</p>
+        <p>Pantalones</p>
+      </div>
+      <div className={styles.help}>
+        <h3>Ayuda</h3>
+        <p>FAQ</p>
+        <p>Devoluciones</p>
+        <p>Servicio al cliente</p>
+      </div>
+      <div className={styles.company}>
+        <h3>Nuestra empresa</h3>
+        <p>Quienes somos</p>
+        <p>Servicios</p>
+        <p>Politica de privacidad</p>
       </div>
     </div>
   );
