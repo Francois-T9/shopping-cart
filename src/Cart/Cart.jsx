af6c5eb494d938037e3d983acc4c4df6f5312efc
Francois <francois.thullier98@gmail.com>
2025-04-24 13:57:04 +0200

    Added bill and total price

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "../Cart/Cart.module.css";
-import styles2 from "../Home/Home.module.css";
 
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
@@ -13,35 +12,73 @@ export default function Cart() {
 
   if (cartArray.length == 0) {
     return (
-      <div className={styles2.home}>
+      <div className={styles.main}>
         <Header />
         <div className={styles.cart}>
-          <h3>Your cart is empty!</h3>
-          <Link to="../shop">
-            {" "}
-            <button>Start shopping!</button>
-          </Link>
+          <div className="cart-items">
+            <h3>Your cart is empty!</h3>
+            <Link to="../shop">
+              {" "}
+              <button>Start shopping!</button>
+            </Link>
+          </div>
+          <div className={styles.bill}>
+            <h3>Bill</h3>
+            <div className="bill-fields">
+              <p>Subtotal</p>
+              <p>0$</p>
+            </div>
+            <div className="bill-fields">
+              <p>Shipping</p>
+              <p>0$</p>
+            </div>
+            <div className="bill-fields">
+              <p>Total</p>
+              <p>0$</p>
+            </div>
+            <button>Checkout</button>
+          </div>
         </div>
         <Footer />
       </div>
     );
   }
   return (
-    <div className={styles2.home}>
+    <div className={styles.main}>
       <Header />
       <div className={styles.cart}>
-        <h3>Your cart: </h3>
-        {cartArray.map((item) => (
-          <CartItem
-            key={item.productName}
-            itemName={item.productName}
-            quantity={item.quantity}
-            price={item.price}
-            removeItem={removeItemFromCart}
-          />
-        ))}
-        <button onClick={removeAllCartItems}>Remove all items</button>
+        <div className={styles.cartItems}>
+          <h3>Your cart: </h3>
+          {cartArray.map((item) => (
+            <CartItem
+              key={item.productName}
+              itemName={item.productName}
+              quantity={item.quantity}
+              price={item.price}
+              removeItem={removeItemFromCart}
+            />
+          ))}
+          <button onClick={removeAllCartItems}>Remove all items</button>
+        </div>
+
+        <div className={styles.bill}>
+          <h3>Bill</h3>
+          <div className="bill-fields">
+            <p>Subtotal</p>
+            <p>0$</p>
+          </div>
+          <div className="bill-fields">
+            <p>Shipping</p>
+            <p>0$</p>
+          </div>
+          <div className="bill-fields">
+            <p>Total</p>
+            <p>0$</p>
+          </div>
+          <button>Checkout</button>
+        </div>
       </div>
+
       <Footer />
     </div>
   );
