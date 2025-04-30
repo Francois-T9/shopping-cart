b215da83498b98d3c485d0e6ff52d846648d9621
Francois <francois.thullier98@gmail.com>
2025-04-30 13:50:24 +0200

    Added div container and passed the updateQuantity function to child

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -8,7 +8,12 @@ import CartItem from "../CartItem/CartItem";
 import { useCart } from "../CartContext";
 
 export default function Cart() {
-  const { cartArray, removeItemFromCart, removeAllCartItems } = useCart();
+  const {
+    cartArray,
+    removeItemFromCart,
+    removeAllCartItems,
+    updateCartQuantity,
+  } = useCart();
   const subtotal = cartArray.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -18,29 +23,31 @@ export default function Cart() {
     return (
       <div>
         <Header />
-        <div className={styles.cart}>
-          <div className="cart-items">
-            <h3>Your cart is empty!</h3>
-            <Link to="../shop">
-              {" "}
-              <button>Start shopping!</button>
-            </Link>
-          </div>
-          <div className={styles.bill}>
-            <h3>Bill</h3>
-            <div className="bill-fields">
-              <p>Subtotal</p>
-              <p>{subtotal}</p>
+        <div className={styles.container}>
+          <div className={styles.cart}>
+            <div className="cart-items">
+              <h3>Your cart is empty!</h3>
+              <Link to="../shop">
+                {" "}
+                <button>Start shopping!</button>
+              </Link>
             </div>
-            <div className="bill-fields">
-              <p>Shipping</p>
-              <p>4.99 $</p>
+            <div className={styles.bill}>
+              <h3>Bill</h3>
+              <div className="bill-fields">
+                <p>Subtotal</p>
+                <p>{subtotal}</p>
+              </div>
+              <div className="bill-fields">
+                <p>Shipping</p>
+                <p>4.99 $</p>
+              </div>
+              <div className="bill-fields">
+                <p>Total</p>
+                <p>0$</p>
+              </div>
+              <button>Checkout</button>
             </div>
-            <div className="bill-fields">
-              <p>Total</p>
-              <p>0$</p>
-            </div>
-            <button>Checkout</button>
           </div>
         </div>
         <Footer />
@@ -50,39 +57,39 @@ export default function Cart() {
   return (
     <div>
       <Header />
-      <div className={styles.cart}>
-        <div className={styles.cartItems}>
-          <h3>Your cart: </h3>
-
-          {cartArray.map((item) => (
-            <CartItem
-              key={item.productName}
-              itemName={item.productName}
-              itemImg={item.srcImg}
-              quantity={item.quantity}
-              price={item.price}
-              removeItem={removeItemFromCart}
-            />
-          ))}
-
-          <button onClick={removeAllCartItems}>Remove all items</button>
-        </div>
-
-        <div className={styles.bill}>
-          <h3>Bill</h3>
-          <div className="bill-fields">
-            <p>Subtotal</p>
-            <p>{subtotal.toFixed(2)} $</p>
+      <div className={styles.container}>
+        <div className={styles.cart}>
+          <div className={styles.cartItems}>
+            <h3>Your cart: </h3>
+            {cartArray.map((item) => (
+              <CartItem
+                key={item.productName}
+                itemName={item.productName}
+                itemImg={item.srcImg}
+                quantity={item.quantity}
+                price={item.price}
+                removeItem={removeItemFromCart}
+                updateCartQuantity={updateCartQuantity}
+              />
+            ))}
+            <button onClick={removeAllCartItems}>Remove all items</button>
           </div>
-          <div className="bill-fields">
-            <p>Shipping</p>
-            <p>4.99 $</p>
+          <div className={styles.bill}>
+            <h3>Bill</h3>
+            <div className="bill-fields">
+              <p>Subtotal</p>
+              <p>{subtotal.toFixed(2)} $</p>
+            </div>
+            <div className="bill-fields">
+              <p>Shipping</p>
+              <p>4.99 $</p>
+            </div>
+            <div className="bill-fields">
+              <p>Total</p>
+              <p>{(subtotal + 4.99).toFixed(2)} $</p>
+            </div>
+            <button>Checkout</button>
           </div>
-          <div className="bill-fields">
-            <p>Total</p>
-            <p>{(subtotal + 4.99).toFixed(2)} $</p>
-          </div>
-          <button>Checkout</button>
         </div>
       </div>
 
