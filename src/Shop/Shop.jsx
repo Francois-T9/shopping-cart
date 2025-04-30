3078b5285bc4cd1cb0bfa8a4e869f6c2d1e0e0b8
Francois <francois.thullier98@gmail.com>
2025-04-30 13:49:14 +0200

    Removed title and added a div container

--- src/Shop/Shop.jsx
+++ src/Shop/Shop.jsx
@@ -14,23 +14,23 @@ export default function Shop({ apiItems }) {
     <div>
       <Header />
 
-      <div className={styles2.main}>
-        <h2>Items</h2>
-        <div className={styles2.itemList}>
-          {apiItems.map((item) => (
-            <Item
-              key={item.id}
-              itemName={item.title.substring(0, 10)}
-              itemPrice={item.price}
-              pathToImg={item.image}
-              onAddToCart={addToCart}
-            ></Item>
-          ))}
-
-          {/* <Link to="../cart">
-            {" "}
-            <button>Go to checkout</button>
-          </Link> */}
+      <div className={styles2.container}>
+        <div className={styles2.main}>
+          <div className={styles2.itemList}>
+            {apiItems.map((item) => (
+              <Item
+                key={item.id}
+                itemName={item.title.substring(0, 10)}
+                itemPrice={item.price}
+                pathToImg={item.image}
+                onAddToCart={addToCart}
+              ></Item>
+            ))}
+            {/* <Link to="../cart">
+              {" "}
+              <button>Go to checkout</button>
+            </Link> */}
+          </div>
         </div>
       </div>
 
