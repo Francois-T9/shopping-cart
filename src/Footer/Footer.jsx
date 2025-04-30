8add1cb38141e125630ea98813f770e92fe2d345
Francois <francois.thullier98@gmail.com>
2025-04-30 13:50:50 +0200

    Changed socials icons

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -3,32 +3,33 @@ import styles from "./Footer.module.css";
 
 export default function Footer() {
   return (
-    <div className={styles.footer}>
-      <div className={styles.socials}>
-        <h3>Redes sociales</h3>
-        <img src="/img/icons8-instagram-24.png" alt="Instagram" />
-        <img src="/img/icons8-tiktok-24.png" alt="TikTok" />
-        <img src="/img/icons8-facebook-24.png" alt="Facebook" />
-
-        <img src="/img/github (1).svg" alt="Twitter" />
-      </div>{" "}
-      <div className={styles.products}>
-        <h3>Nuestros productos</h3>
-        <p>Té matcha</p>
-        <p>Macarrons</p>
-        <p>Café gourmand</p>
-      </div>
-      <div className={styles.help}>
-        <h3>Ayuda</h3>
-        <p>FAQ</p>
-        <p>Devoluciones</p>
-        <p>Servicio al cliente</p>
-      </div>
-      <div className={styles.company}>
-        <h3>Nuestra empresa</h3>
-        <p>Quienes somos</p>
-        <p>Servicios</p>
-        <p>Politica de privacidad</p>
+    <div className={styles.container}>
+      <div className={styles.footer}>
+        <div className={styles.socials}>
+          <h3>Redes sociales</h3>
+          <img src="/img/instagram (1).svg" alt="Instagram" />
+          <img src="/img/linkedin (1).svg" alt="TikTok" />
+          <img src="/img/facebook.svg" alt="Facebook" />
+          <img src="/img/github (1).svg" alt="Twitter" />
+        </div>{" "}
+        <div className={styles.products}>
+          <h3>Nuestros productos</h3>
+          <p>Té matcha</p>
+          <p>Macarrons</p>
+          <p>Café gourmand</p>
+        </div>
+        <div className={styles.help}>
+          <h3>Ayuda</h3>
+          <p>FAQ</p>
+          <p>Devoluciones</p>
+          <p>Servicio al cliente</p>
+        </div>
+        <div className={styles.company}>
+          <h3>Nuestra empresa</h3>
+          <p>Quienes somos</p>
+          <p>Servicios</p>
+          <p>Politica de privacidad</p>
+        </div>
       </div>
     </div>
   );
